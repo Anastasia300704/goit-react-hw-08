bff96da7f446826f7b5ae9dc3166bfad89c2fb98
Anastasia Makarova <anastasiyushka30@gmail.com>
2025-03-09 16:14:12 +0300

    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -11,9 +11,9 @@ import { BrowserRouter } from 'react-router-dom';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
-      <BrowserRouter basename="/goit-react-hw-08">
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </PersistGate>
   </Provider>
-);
+); 
