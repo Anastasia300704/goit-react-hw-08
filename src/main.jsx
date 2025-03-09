3bdfb4a291f735028f02e14075f23dbdfe02b083
Anastasia Makarova <anastasiyushka30@gmail.com>
2025-03-09 15:26:50 +0300

    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-
-import { Provider } from 'react-redux';
-import { store, persistor } from './redux/store';
-import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
-      <BrowserRouter>
+      <BrowserRouter basename="/goit-react-hw-08">
         <App />
       </BrowserRouter>
     </PersistGate>
